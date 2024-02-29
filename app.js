d0f3d47fa61de7c0ea0842da6d2b375a138ec47c
talhahzahid <talhazahid415@gmail.com>
2024-02-29 17:54:53 +0500

    index file

---
+++ app.js
@@ -0,0 +1,120 @@
+
+
+
+
+// var percentage = +prompt("Enter Your Percentage");
+// if (percentage > 100) {
+//     console.log("abey loro");
+// } else if (percentage < 40) {
+//     console.log("you are Eliminate Better Luck Next Time");
+// } 
+// // else if (percentage <= 50) {
+// //     console.log("congratulation you are passed Keep Improving");
+// // }
+// //  else if (percentage < 60) {
+// //     console.log("congratulation you grade is B");
+// // }
+// //  else if (percentage <= 50) {
+// //     console.log("congratulation you are passed Keep Improving");
+// // }
+// //  else if (percentage <= 50) {
+// //     console.log("congratulation you are passed Keep Improving");
+// // }
+// else {
+//     console.log("Please Input Correct Value");
+// }
+
+
+
+
+
+
+
+
+
+// let num = 5;
+// let result = (num % 2 === 0) ? "Even" : "Odd";
+// console.log(result);
+
+
+
+// let num1 = 4;
+// if ( num1 % 12 != 2){
+//     console.log("correct")
+// }else{
+//     console.log("Incorreect")
+// };
+
+// question no 3  
+
+// let age = +prompt("Enter Your Age20");
+
+// if (age > 18) {
+//     console.log("Old enough");
+// } else {
+//     console.log("Too Young");
+// }
+
+// Question no 4 
+
+// var username = prompt("Enter your Name");
+// var _name = "Developer";
+// if(username === _name){
+//     console.log("Welcome Sir");
+// }else{
+//     console.log("Incorrect Username");
+// }
+
+// Question no 5 
+
+// var num = 2;
+// if ( 3 % num){
+//     console.log("Dividible by 3")
+// }else{
+//     console.log("Not Dividible by 3")
+// }
+
+// let number = parseInt(prompt("Enter a number:"));
+
+// switch (number % 3) {
+//     case 0:
+//         console.log("Number is divisible by 3");
+//         break;
+//     default:
+//         console.log("Number is not divisible by 3");
+// }
+
+
+// var num = +prompt("Enter Your Number");
+// if (num % 3){
+//     console.log("Dividible by 3")
+// }else{
+//     console.log("Not Dividible by 3")
+// }
+
+
+
+// Question no 12 
+
+// var hour = 13;
+// if (hour < 18) {
+// console.log("Good day");}
+// else{
+// console.log("Good evening");
+// } 
+
+// question no 14 
+ 
+// let num11 = parseFloat(prompt("Enter a number: "));
+ 
+
+
+// question no 14 
+// var inputNum = +prompt("Enter a number");
+// if ( inputNum > 0){
+//     console.log("POSITIVE NUMBER");
+// }else if( inputNum < 0){
+//     console.log("NAGATIVE NUMBER");
+// }else{
+//     console.log("THE NUMBER IS ZERO");
+// }
